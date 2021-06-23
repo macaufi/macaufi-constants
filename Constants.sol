2d72153c54130617d14c88b63c7f3eb546b165c2
VelKovacevic <46655240+VelKovacevic@users.noreply.github.com>
2021-06-23 15:03:54 +0200

    Create Constants.sol

---
+++ Constants.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.6.0 <0.7.0;
+
+library Constants {
+  address public constant SUPER_PRIZE_BSC =  0xE0f5206BBD039e7b0592d8918820024e2a7437b9;
+}
